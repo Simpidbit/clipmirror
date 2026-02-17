4b86c57f00495a6c3f85be2fef4a55478f968460
Simpidbit Isaiah <simpidbit@gmail.com>
2026-02-17 14:43:46 +0800

    First commit

---
+++ client.py
@@ -0,0 +1,116 @@
+import pyperclip
+import time
+import socket
+import sys
+
+IP = '127.0.0.1'
+PORT = 8080
+
+
+class ClipboardMonitor:
+    def __init__(self) -> None:
+        self.current_paste = str()
+
+    def destruct(self) -> None:
+        pass
+
+    def check_update(self):
+        if self.current_paste is None:
+            self.current_paste = pyperclip.paste()
+            return False
+        else:
+            new_paste = pyperclip.paste()
+            if new_paste != self.current_paste:
+                self.current_paste = new_paste
+                return True
+    # 格式:
+    # | ID长度(2字节) | ID | paste长度(4字节) | paste
+    def make_msg(self, identity:str) -> bytes:
+        msg = int(len(identity)).to_bytes(2) + \
+              identity.encode('utf-8') + \
+              int(len(self.current_paste)).to_bytes(4) + \
+              self.current_paste.encode('utf-8')
+        return msg
+
+
+# 报文格式:
+# | 四位数字 | 消息
+class MessageMonitor:
+    def __init__(self) -> None:
+        self.paste = str()
+        self.raw = bytes()
+        self.raw_length = 0
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((IP, PORT))
+        self.socket.setblocking(False)
+
+    def destruct(self) -> None:
+        self.socket.close()
+
+    def get_paste(self) -> str:
+        return self.paste
+
+    def load_raw(self, raw) -> bool:
+        self.raw += raw
+        self.raw_length += len(raw)
+        if self.raw_length >= 4:
+            paste_length = int.from_bytes(self.raw[:4])
+        else:
+            return False
+        
+        if len(self.raw) >= 4 + paste_length:
+            self.paste = self.raw[4 : 4 + paste_length].decode('utf-8')
+            self.raw = self.raw[4 + paste_length:]
+            self.raw_length -= 4 + paste_length
+            return True
+        else:
+            return False
+
+    def check_msg(self) -> bool:
+        try:
+            data = self.socket.recv(4096)
+
+            if not data:
+                # Connection was closed
+                self.socket.close()
+                sys.exit(1)
+
+            return self.load_raw(data)
+        except BlockingIOError:
+            self.socket.close()
+            sys.exit(2)
+        except ConnectionResetError:
+            self.socket.close()
+            sys.exit(3)
+
+    def send_msg(self, msg:bytes) -> None:
+        self.socket.send(msg)
+
+    def get_id(self) -> str:
+        sockname = self.socket.getsockname()
+        return f'{sockname[0]}:{sockname[1]}'
+
+
+class EventMonitor:
+    def __init__(self) -> None:
+        self.clipboard_monitor = ClipboardMonitor()
+        self.message_monitor = MessageMonitor()
+
+    def start_loop(self):
+        try:
+            while True:
+                if self.clipboard_monitor.check_update():
+                    self.message_monitor.send_msg(
+                        self.clipboard_monitor.make_msg(self.message_monitor.get_id())
+                    )
+                elif self.message_monitor.check_msg():
+                    pyperclip.copy(self.message_monitor.get_paste())
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            self.message_monitor.destruct()
+            self.clipboard_monitor.destruct()
+
+if __name__ == '__main__':
+    monitor = ClipboardMonitor()
+    monitor.check_update()
---
+++ server.py
@@ -0,0 +1,106 @@
+import socket
+import select
+import threading
+
+PORT = 8080
+
+def sendall(s:socket.socket, msg:bytes) -> None:
+    s.sendall(msg)
+
+class MessageFromClient:
+    def __init__(self, hostid:str, paste:str):
+        self.hostid = hostid
+        self.paste = paste
+
+    def make_msg_to_client(self) -> bytes:
+        return int(len(self.paste)).to_bytes(4) + self.paste.encode('utf-8')
+
+
+class MessageParser:
+    def __init__(self) -> None:
+        self.raw = bytes()
+        self.raw_length = int()
+
+    def load(self, msg:bytes) -> None | MessageFromClient:
+        self.raw += msg
+        self.raw_length += len(msg)
+
+        try:
+            hostid_length = int.from_bytes(self.raw[:2])
+            hostid = self.raw[2 : 2 + hostid_length].decode('utf-8')
+            paste_length = int.from_bytes(self.raw[2 + hostid_length : 2 + hostid_length + 4])
+            paste = self.raw[2 + hostid_length + 4:].decode('utf-8')
+
+            complete_msg_length = 2 + hostid_length + 4 + paste_length
+            if len(self.raw) < complete_msg_length:
+                raise IndexError
+            else:
+                self.raw = self.raw[complete_msg_length:]
+                self.raw_length -= complete_msg_length
+                return MessageFromClient(hostid, paste)
+        except IndexError:
+            return None
+
+class MessagePeeker:
+    def __init__(self) -> None:
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setblocking(False)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind(('0.0.0.0', PORT))
+        self.server_socket.listen(32)
+        self.inputs = [self.server_socket]
+        self.parsers = [MessageParser()]
+        
+
+    def peek_message(self) -> list:
+        loaded_msgs = list()
+        rs, _, es = select.select(self.inputs, [], self.inputs)
+        for ri in range(len(self.inputs)):
+            if not (self.inputs[ri] in rs): continue
+            if self.inputs[ri] is self.server_socket:
+                # 新的客户端连接
+                conn, addr = self.server_socket.accept()
+                conn.setblocking(False)
+                self.inputs.append(conn)
+                self.parsers.append(MessageParser())
+            else:
+                # 已有客户端发来的消息
+                try:
+                    data = self.inputs[ri].recv(4096)
+                    if data:
+                        msg_from_client = self.parsers[ri].load(self.inputs[ri].recv(4096))
+                        if msg_from_client is not None:
+                            loaded_msgs.append(msg_from_client)
+                    else:
+                        raise ConnectionResetError
+                except ConnectionResetError:
+                    # 客户端断开
+                    self.inputs[ri].close()
+                    self.inputs.pop(ri)
+                    self.parsers.pop(ri)
+
+        return loaded_msgs
+
+    def broadcast(self, msg:MessageFromClient) -> None:
+        for cs in self.inputs[1:]:
+            peername = cs.getpeername()
+            peerid = f'{peername[0]}:{peername[1]}'
+            if peerid != msg.hostid:
+                threading.Thread(target = sendall, args = (cs, msg.make_msg_to_client())).start()
+
+
+class App:
+    def __init__(self) -> None:
+        self.msg_peeker = MessagePeeker()
+
+    def start_loop(self) -> None:
+        while True:
+            loaded_msgs = self.msg_peeker.peek_message()
+            if len(loaded_msgs) == 0:
+                continue
+            self.msg_peeker.broadcast(loaded_msgs[-1])
+
+
+if __name__ == '__main__':
+    app = App()
+    app.start_loop()
